7822e8783e3fe3a0ae30f3da805a2d0b780cae38
anandsunku <anand.sunku@gmail.com>
2014-06-02 07:22:17 +0530

    second workspace
    second workspace

---
+++ completeref/completeref/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  completeref
+//
+//  Created by anand on 02/06/14.
+//  Copyright (c) 2014 AnandSunku. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    //this is good
+    return 0;
+}
+
