9bd70e3c80b8187863daa75d24ca3bc1274de314
anandsunku <anand.sunku@gmail.com>
2014-06-02 19:34:45 +0530

    sizeof checking
    size of structures

---
+++ howmuch/howmuch/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  howmuch
+//
+//  Created by anand on 02/06/14.
+//  Copyright (c) 2014 AnandSunku. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    cout << "Hello, World!\n";
+    
+    cout<< "jai vinayaka"<<endl;
+
+    return 0;
+}
+
