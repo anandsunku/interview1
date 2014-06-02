90dc62d25e5cd408c801bb53c25de0a6bf002dda
anandsunku <anand.sunku@gmail.com>
2014-06-02 20:14:48 +0530

    sizeof operator more example
    sizeof operator more example

--- howmuch/howmuch/main.cpp
+++ howmuch/howmuch/main.cpp
@@ -10,14 +10,79 @@
 
 using namespace std;
 
+class nobody {
+};
+
+class character {
+    char value;
+};
+class integers{
+    int intval;
+};
+
+class doubles{
+    double dvalue;
+};
+
+class fewmembers {
+    char value;
+    int secondone;
+    char value1;
+    double thirdone;
+};
+
+class diffmembers {
+    char value;
+    int secondone;
+    char value1;
+    double thirdone;
+};
+
+class withstatic :public integers{
+public:
+    static int  value;
+    
+    static void showoff();
+    
+    char value1;
+};
+
+void withstatic::showoff(){
+    cout<<"this is too much "<<endl;
+}
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    cout << "Hello, World!\n";
-    
     cout<< "jai vinayaka"<<endl;
+    
+    nobody ob1;
+    character ob2;
+    integers ob4;
+    doubles ob5;
+    withstatic ob6;
+    
+    fewmembers ob10;
+    diffmembers ob11;
 
+    
+    printf("size of nobody = %ld object size = %ld\n", sizeof(nobody),sizeof(ob1));
+    
+    printf("size of character = %ld  object size = %ld\n", sizeof(character), sizeof(ob2));
+
+    printf("size of integers = %ld  object size = %ld\n", sizeof(integers), sizeof(ob4));
+
+    printf("size of doubles = %ld  object size = %ld\n", sizeof(doubles), sizeof(ob5));
+    
+    printf("size of withstatic = %ld  object size = %ld\n", sizeof(withstatic), sizeof(ob6));
+    
+    printf("more space \n\n\n");
+    
+    printf("size of fewmembers should be = %ld \n", sizeof(char)+sizeof(int)+sizeof(char)+sizeof(double));
+    
+    printf("size of fewmembers = %ld  object size = %ld\n", sizeof(fewmembers), sizeof(ob10));
+    
+    printf("size of diffmembers = %ld  object size = %ld\n", sizeof(diffmembers), sizeof(ob11));
+    
     return 0;
 }
 
